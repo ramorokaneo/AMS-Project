a82764db9eb70053cb15b77faae5d8b0c8b58056
Mbali <mbalikhumalo408@gmail.com>
2023-11-20 11:40:02 +0200

    mediaScreen

--- src/Components/Client/SignUp/SignUp.jsx
+++ src/Components/Client/SignUp/SignUp.jsx
@@ -1,46 +1,3 @@
-// import React from 'react';
-// import { Box, Paper, TextField, Button, Typography } from '@mui/material';
-
-// const SignUp = () => {
-//     return (
-//         <Box
-//             sx={{
-//                 minHeight: '100vh',
-//                 display: 'flex',
-//                 justifyContent: 'center',
-//                 alignItems: 'center',
-//                 padding: '20px',
-//             }}
-//         >
-//             <Paper sx={{ padding: '40px', maxWidth: '400px', marginRight: 'auto' }}>
-//                 <Typography variant="h5" align="left" gutterBottom>
-//                     Sign Up
-//                 </Typography>
-
-//                 <form>
-//                     <TextField id='standard-basic' label="Email" variant='standard' fullWidth size='small' required />
-//                     <p>
-//                         <TextField id="outlined-password-input" label="Password" type="password"  autoComplete="current-password" variant="standard" required/>
-//                     </p>
-//                     <Button
-//                         type="submit"
-//                         variant="contained"
-//                         color="primary"
-//                         fullWidth
-//                         sx={{ mt: 2 }}
-//                     >
-//                         Sign Up
-//                     </Button>
-//                 </form>
-//             </Paper>
-//         </Box>
-//     );
-// };
-
-// export default SignUp;
-
-
-
 import React from 'react';
 import { Box, Paper, TextField, Button, Typography } from '@mui/material';
 
@@ -60,9 +17,15 @@ const SignUp = () => {
                     padding: '40px',
                     maxWidth: '400px',
                     marginRight: 'auto',
+                    '@media (max-width: 1024px)': {
+                        // Styles for desktop screens
+                        maxWidth: '500px',
+                        padding: '60px',
+                    },
                     '@media (max-width: 768px)': {
                         // Styles for tablets and smaller screens (e.g., laptops)
                         maxWidth: '300px',
+                        padding: '40px',
                     },
                     '@media (max-width: 480px)': {
                         // Styles for mobile devices
