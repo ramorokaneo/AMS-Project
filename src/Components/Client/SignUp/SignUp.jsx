2c3019428936c14ded12832013d50a8f945dcae7
Mbali <mbalikhumalo408@gmail.com>
2023-11-20 12:51:18 +0200

    new

--- src/Components/Client/SignUp/SignUp.jsx
+++ src/Components/Client/SignUp/SignUp.jsx
@@ -1,72 +1,52 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import {
-    Container,
-    Grid,
-    Paper,
-    TextField,
-    Button,
-    Typography,
-} from '@material-ui/core';
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-        padding: theme.spacing(2),
-    },
-    paper: {
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    },
-    form: {
-        display: 'flex',
-        flexDirection: 'column',
-        '& .MuiTextField-root': {
-            margin: theme.spacing(1),
-            width: '100%',
-        },
-    },
-}));
+import { Box, Paper, TextField, Button, Typography } from '@mui/material';
 
 const SignUp = () => {
-    const classes = useStyles();
-
     return (
-        <div className={classes.root}>
-            <Container maxWidth="md">
-                <Paper className={classes.paper}>
-                    <Typography variant="h4" gutterBottom>
+        <Box
+            sx={{
+                minHeight: '100vh',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                padding: '20px',
+            }}
+        >
+            <Paper sx={{ padding: '40px', maxWidth: '400px' }}>
+                <Typography variant="h4" align="center" gutterBottom>
+                    Sign Up
+                </Typography>
+
+                <form>
+                    <TextField
+                        fullWidth
+                        label="Email"
+                        variant="outlined"
+                        margin="normal"
+                        required
+                    />
+
+                    <TextField
+                        fullWidth
+                        label="Password"
+                        variant="outlined"
+                        margin="normal"
+                        type="password"
+                        required
+                    />
+
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        fullWidth
+                        sx={{ mt: 2 }}
+                    >
                         Sign Up
-                    </Typography>
-                    <form className={classes.form} noValidate autoComplete="off">
-                        <TextField
-                            required
-                            id="outlined-required"
-                            label="Username"
-                            variant="outlined"
-                        />
-                        <TextField
-                            required
-                            id="outlined-password-input"
-                            label="Password"
-                            type="password"
-                            variant="outlined"
-                        />
-                        <TextField
-                            required
-                            id="outlined-email-input"
-                            label="Email"
-                            type="email"
-                            variant="outlined"
-                        />
-                        <Button variant="contained" color="primary">
-                            Sign Up
-                        </Button>
-                    </form>
-                </Paper>
-            </Container>
-        </div>
+                    </Button>
+                </form>
+            </Paper>
+        </Box>
     );
 };
 
