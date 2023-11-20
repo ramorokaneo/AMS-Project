49fb00b302fae550fde329e9f02e1205aaa25c44
Mbali <mbalikhumalo408@gmail.com>
2023-11-20 12:37:43 +0200

    line

--- src/Components/Client/SignUp/SignUp.jsx
+++ src/Components/Client/SignUp/SignUp.jsx
@@ -2,78 +2,77 @@ import React from 'react';
 import { Box, Paper, TextField, Button, Typography } from '@mui/material';
 
 const SignUp = () => {
-  return (
-    <Box
-      sx={{
-        minHeight: 'calc(100vh - 40px)',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: '20px',
-      }}
-    >
-      <Paper
-        sx={{
-          padding: '40px',
-          width: '100%',
-          maxWidth: '400px',
-          marginRight: 'auto',
-          marginLeft: '1000px', 
-          '@media (max-width: 1024px)': {
-            // Styles for desktop screens
-            maxWidth: '500px',
-            padding: '60px',
-          },
-          '@media (max-width: 768px)': {
-            // Styles for tablets and smaller screens (e.g., laptops)
-            maxWidth: '300px',
-            padding: '40px',
-          },
-          '@media (max-width: 480px)': {
-            // Styles for mobile devices
-            maxWidth: '250px',
-            padding: '20px',
-          },
-        }}
-      >
-        <Typography variant="h5" align="left" gutterBottom>
-          Sign Up
-        </Typography>
-
-        <form>
-          <TextField
-            id="standard-basic"
-            label="Email"
-            variant="standard"
-            fullWidth
-            size="small"
-            required
-          />
-          <p>
-            <TextField
-              id="outlined-password-input"
-              label="Password"
-              type="password"
-              autoComplete="current-password"
-              variant="standard"
-              required
-            />
-          </p>
-          <Button
-            type="submit"
-            variant="contained"
-            fullWidth
+    return (
+        <Box
             sx={{
-              mt: 2,
-              backgroundColor: '#000044',
+                minHeight: '100px',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                padding: '20px',
             }}
-          >
-            SIGN UP
-          </Button>
-        </form>
-      </Paper>
-    </Box>
-  );
+        >
+            <Paper
+                sx={{
+                    padding: '40px',
+                    width: '100%', 
+                    maxWidth: '400px',
+                    marginRight: '-675%',
+                    '@media (max-width: 1024px)': {
+                        // Styles for desktop screens
+                        maxWidth: '500px',
+                        padding: '60px',
+                    },
+                    '@media (max-width: 768px)': {
+                        // Styles for tablets and smaller screens (e.g., laptops)
+                        maxWidth: '300px',
+                        padding: '40px',
+                    },
+                    '@media (max-width: 480px)': {
+                        // Styles for mobile devices
+                        maxWidth: '250px',
+                        padding: '20px',
+                    },
+                }}
+            >
+                <Typography variant="h5" align="left" gutterBottom>
+                    Sign Up
+                </Typography>
+
+                <form>
+                    <TextField
+                        id="standard-basic"
+                        label="Email"
+                        variant="standard"
+                        fullWidth
+                        size="small"
+                        required
+                    />
+                    <p>
+                        <TextField
+                            id="outlined-password-input"
+                            label="Password"
+                            type="password"
+                            autoComplete="current-password"
+                            variant="standard"
+                            required
+                        />
+                    </p>
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        fullWidth
+                        sx={{
+                            mt: 2,
+                            backgroundColor: '#000044',
+                        }}
+                    >
+                        SIGN UP
+                    </Button>
+                </form>
+            </Paper>
+        </Box>
+    );
 };
 
 export default SignUp;
