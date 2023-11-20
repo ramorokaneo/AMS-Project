e68fa3ad806c478f1e5bceb3dc2c4e0ac8061d78
Mbali <mbalikhumalo408@gmail.com>
2023-11-20 13:00:03 +0200

    fresh

--- src/Components/Client/SignUp/SignUp.jsx
+++ src/Components/Client/SignUp/SignUp.jsx
@@ -1,6 +1,43 @@
-import React from 'react';
-import { Box, Paper, TextField, Button, Typography } from '@mui/material';
+// import React from 'react';
+// import { Box, Paper, TextField, Button, Typography } from '@mui/material';
 
+// const SignUp = () => {
+//     return (
+//         <Box
+//             sx={{
+//                 minHeight: '100vh',
+//                 display: 'flex',
+//                 justifyContent: 'center',
+//                 alignItems: 'center',
+//                 padding: '20px',
+//             }}
+//         >
+//             <Paper sx={{ padding: '40px', maxWidth: '400px', fontWeight: 'bold' }}>
+//                 <Typography variant="h7" align="left" gutterBottom>
+//                     SIGN UP
+//                 </Typography>
+
+//                 <form>
+//                     <TextField id='standard-basic' label="Email" variant='standard' fullWidth size='small' required />
+
+//                     <TextField fullWidth id='standard-basic' label="Password" variant='standard' size='small' type="password" required />
+
+//                     <Button type="submit" variant="contained" color="primary" fullWidth sx={{ mt: 2, backgroundColor: '#030539' }}   >
+//                         Sign Up
+//                     </Button>
+//                 </form>
+//             </Paper>
+//         </Box>
+//     );
+// };
+
+// export default SignUp;
+
+
+
+import React from 'react';
+import { Box, Paper, TextField, Button, Typography, Grid, Icon } from '@mui/material';
+import PersonAddIcon from '@mui/icons-material/PersonAdd'; 
 const SignUp = () => {
     return (
         <Box
@@ -12,36 +49,18 @@ const SignUp = () => {
                 padding: '20px',
             }}
         >
-            <Paper sx={{ padding: '40px', maxWidth: '400px' }}>
-                <Typography variant="h4" align="center" gutterBottom>
+            <Paper sx={{ padding: '40px', maxWidth: '400px', fontWeight: 'bold' }}>
+                <Icon component={PersonAddIcon} fontSize="large" color="primary" />
+                <Typography variant="h6" align="center" gutterBottom>
                     Sign Up
                 </Typography>
 
                 <form>
-                    <TextField
-                        fullWidth
-                        label="Email"
-                        variant="outlined"
-                        margin="normal"
-                        required
-                    />
+                    <TextField id='standard-basic' label="Email" variant='standard' fullWidth size='small' required />
 
-                    <TextField
-                        fullWidth
-                        label="Password"
-                        variant="outlined"
-                        margin="normal"
-                        type="password"
-                        required
-                    />
+                    <TextField fullWidth id='standard-basic' label="Password" variant='standard' size='small' type="password" required />
 
-                    <Button
-                        type="submit"
-                        variant="contained"
-                        color="primary"
-                        fullWidth
-                        sx={{ mt: 2 }}
-                    >
+                    <Button type="submit" variant="contained" color="primary" fullWidth sx={{ mt: 2, backgroundColor: '#030539' }}   >
                         Sign Up
                     </Button>
                 </form>
