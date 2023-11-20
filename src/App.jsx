d8c01b2016661c96cf66fa12ac33a815eeaac619
Mbali <mbalikhumalo408@gmail.com>
2023-11-20 16:45:40 +0200

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignUp from './Components/Client/SignUp/SignUp';
+import AccountHolder from './Components/Client/SignUp/AccountHolder';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SignUp />} />
-        <Route path='accountholder' element={<AccountHolder />} />
-        <Route path='/altcontact' element={<AltContact />} />
+        <Route path='/accountholder' element={<AccountHolder/>}/>
       </Routes>
     </BrowserRouter>
   );
--- src/Components/Client/SignUp/AccountHolder.jsx
+++ src/Components/Client/SignUp/AccountHolder.jsx
@@ -8,5 +8,4 @@ const AccountHolder = () => {
     )
 };
 
-
 export default AccountHolder
