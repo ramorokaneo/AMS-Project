71566c4f703d3757e42dacf38e1135ce581f11d9
Mbali <mbalikhumalo408@gmail.com>
2023-11-20 16:49:32 +0200

    new

--- src/Components/Client/SignUp/AccountHolder.jsx
+++ src/Components/Client/SignUp/AccountHolder.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import { Paper } from "@mui/material";
 
 const AccountHolder = () => {
     return (
-        <>
-        </>
-    )
+        <Paper elevation={1} style={{ padding: 20 }}>
+        </Paper>
+    );
 };
 
-export default AccountHolder
+export default AccountHolder;
