d89a0dcc02039bfbad1b8b447e9dafa9ecbe1516
Mbali <mbalikhumalo408@gmail.com>
2023-11-20 11:36:36 +0200

    mediascreen

--- src/Components/Client/SignUp/SignUp.jsx
+++ src/Components/Client/SignUp/SignUp.jsx
@@ -1,3 +1,46 @@
+// import React from 'react';
+// import { Box, Paper, TextField, Button, Typography } from '@mui/material';
+
+// const SignUp = () => {
+//     return (
+//         <Box
+//             sx={{
+//                 minHeight: '100vh',
+//                 display: 'flex',
+//                 justifyContent: 'center',
+//                 alignItems: 'center',
+//                 padding: '20px',
+//             }}
+//         >
+//             <Paper sx={{ padding: '40px', maxWidth: '400px', marginRight: 'auto' }}>
+//                 <Typography variant="h5" align="left" gutterBottom>
+//                     Sign Up
+//                 </Typography>
+
+//                 <form>
+//                     <TextField id='standard-basic' label="Email" variant='standard' fullWidth size='small' required />
+//                     <p>
+//                         <TextField id="outlined-password-input" label="Password" type="password"  autoComplete="current-password" variant="standard" required/>
+//                     </p>
+//                     <Button
+//                         type="submit"
+//                         variant="contained"
+//                         color="primary"
+//                         fullWidth
+//                         sx={{ mt: 2 }}
+//                     >
+//                         Sign Up
+//                     </Button>
+//                 </form>
+//             </Paper>
+//         </Box>
+//     );
+// };
+
+// export default SignUp;
+
+
+
 import React from 'react';
 import { Box, Paper, TextField, Button, Typography } from '@mui/material';
 
@@ -12,15 +55,44 @@ const SignUp = () => {
                 padding: '20px',
             }}
         >
-            <Paper sx={{ padding: '40px', maxWidth: '400px', marginRight: 'auto' }}>
+            <Paper
+                sx={{
+                    padding: '40px',
+                    maxWidth: '400px',
+                    marginRight: 'auto',
+                    '@media (max-width: 768px)': {
+                        // Styles for tablets and smaller screens (e.g., laptops)
+                        maxWidth: '300px',
+                    },
+                    '@media (max-width: 480px)': {
+                        // Styles for mobile devices
+                        maxWidth: '250px',
+                        padding: '20px',
+                    },
+                }}
+            >
                 <Typography variant="h5" align="left" gutterBottom>
                     Sign Up
                 </Typography>
 
                 <form>
-                    <TextField id='standard-basic' label="Email" variant='standard' fullWidth size='small' required />
+                    <TextField
+                        id="standard-basic"
+                        label="Email"
+                        variant="standard"
+                        fullWidth
+                        size="small"
+                        required
+                    />
                     <p>
-                        <TextField id="outlined-password-input" label="Password" type="password"  autoComplete="current-password" variant="standard" required/>
+                        <TextField
+                            id="outlined-password-input"
+                            label="Password"
+                            type="password"
+                            autoComplete="current-password"
+                            variant="standard"
+                            required
+                        />
                     </p>
                     <Button
                         type="submit"
