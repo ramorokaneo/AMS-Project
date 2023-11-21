1a98d5ad07a49b818087204e0c05e62d22a4affe
Mbali <mbalikhumalo408@gmail.com>
2023-11-21 08:39:56 +0200

    Holder

--- src/Components/Client/SignUp/AccountHolder.jsx
+++ src/Components/Client/SignUp/AccountHolder.jsx
@@ -4,67 +4,35 @@ import logoImage from '../../images/logo.png';
 
 const AccountHolder = () => {
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                minHeight: '100vh',
-                padding: '20px',
-            }}
+        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh', padding: '20px', }}
         >
-            <Grid container justifyContent="center" spacing={2}>
+            <Grid container justifyContent="flex-end" spacing={2}>
                 <Grid item xs={12} sm={8} md={6} lg={4}>
                     <Paper sx={{ padding: '20px', textAlign: 'center' }}>
                         <img src={logoImage} alt="Logo" style={{ width: '100px' }} />
-                        <Typography variant="h5" sx={{ mt: 2 }} gutterBottom>
-                            Tell Us About Yourself
-                        </Typography>
+
+
+                        <Box>
+                            <Typography variant="subtitle1" sx={{ textAlign: 'left', fontSize: '14px', fontWeight: 'bold' }} gutterBottom>
+                                Main Account Holder
+                            </Typography>
+                            <Typography variant="subtitle1" sx={{ textAlign: 'left', fontSize: '17px', marginTop: '1px', fontWeight: 'bold' }} gutterBottom>
+                                TELL US ABOUT YOURSELF
+                            </Typography>
+                        </Box>
 
                         <form>
-                            <TextField
-                                fullWidth
-                                label="Full Name"
-                                variant="outlined"
-                                margin="normal"
-                                required
-                            />
+                            <TextField fullWidth label="Full Name" variant="standard" margin="normal" />
 
-                            <TextField
-                                fullWidth
-                                label="Email"
-                                variant="outlined"
-                                margin="normal"
-                                required
-                            />
+                            <TextField fullWidth label="Email" variant="standard" margin="normal" required />
 
-                            <TextField
-                                fullWidth
-                                label="Phone Number"
-                                variant="outlined"
-                                margin="normal"
-                                required
-                            />
+                            <TextField fullWidth label="Phone Number" variant="outlined" margin="normal" required />
 
-                            <TextField
-                                fullWidth
-                                label="Address"
-                                variant="outlined"
-                                margin="normal"
-                                multiline
-                                rows={3}
-                                required
-                            />
+                            <TextField fullWidth label="Address" variant="outlined" margin="normal" multiline rows={3} required />
 
-                            <Button
-                                type="submit"
-                                variant="contained"
-                                color="primary"
-                                fullWidth
-                                sx={{ mt: 2 }}
-                            >
-                                Submit
-                            </Button>
+                            <Button type="submit" variant="contained" color="primary" fullWidth sx={{ mt: 2 }} > Submit </Button>
+
+
                         </form>
                     </Paper>
                 </Grid>
